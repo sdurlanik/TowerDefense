598c53ed81830e1aeb6308abfc2ccdcacb212099
Samed Durlanık <sametdurlanik@gmail.com>
2021-06-24 19:20:30 +0300

    Create Pause menu
    Duraklama menüsü ve animasyonları eklendi.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private bool doMovement = true;
     
     public float panSpeed = 30f;
     public float panBorderThickness = 10f;
@@ -16,14 +15,6 @@ public class CameraController : MonoBehaviour
         if (GameManagement.GameIsOver)
         {
             this.enabled = false;
-            return;
-        }
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            doMovement = !doMovement;
-        }
-        if (!doMovement)
-        {
             return;
         }
         if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
