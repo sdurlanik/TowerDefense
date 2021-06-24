bdda0d3555d64c70b86a1aa2f13f14d21f70b738
Samed Durlanık <sametdurlanik@gmail.com>
2021-06-24 18:46:06 +0300

    Add Sell system
    Kule satma sistemi eklendi.

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -95,6 +95,18 @@ public class Node : MonoBehaviour
         isUpgraded = true;
     }
 
+
+    public void SellTurret()
+    {
+        PlayerStats.Money += turretBlueprint.GetSellAmount();
+        
+        GameObject effect = (GameObject)Instantiate(buildManager.sellEffect, GetBuildPosition(), Quaternion.identity);
+        Destroy(effect, 5f);
+        
+        Destroy(turret);
+        turretBlueprint = null;
+    }
+    
     void OnMouseEnter()
     {
         if (EventSystem.current.IsPointerOverGameObject())
