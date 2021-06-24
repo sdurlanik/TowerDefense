4979be8f9295445fb53887ff129d4a5b69ecda3b
Samed Durlanık <sametdurlanik@gmail.com>
2021-06-24 21:44:00 +0300

    Create Main menu
    Ana menü ve animasyonları oluşturuldu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string levelToLoad = "Mainlevel";
+    
+    public void Play()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
